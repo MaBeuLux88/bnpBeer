94e3fdf2f7eca40ee53c2722d9cdcb4db7bc19d4
Maxime_Beugnet <maxime.beugnet@zenika.com>
2015-04-17 02:56:41 +0200

    commit lol

--- beerController.js
+++ beerController.js
@@ -5,23 +5,23 @@ angular.module('beerApp', [])
       {text:'build an angular app', done:false}];
  
     $scope.addTodo = function() {
-      todoList.todos.push({text:todoList.todoText, done:false});
-      todoList.todoText = '';
+      $scope.todos.push({text:$scope.todoText, done:false});
+      $scope.todoText = '';
     };
  
     $scope.remaining = function() {
       var count = 0;
-      angular.forEach(todoList.todos, function(todo) {
+      angular.forEach($scope.todos, function(todo) {
         count += todo.done ? 0 : 1;
       });
       return count;
     };
  
     $scope.archive = function() {
-      var oldTodos = todoList.todos;
-      todoList.todos = [];
+      var oldTodos = $scope.todos;
+      $scope.todos = [];
       angular.forEach(oldTodos, function(todo) {
-        if (!todo.done) todoList.todos.push(todo);
+        if (!todo.done) $scope.todos.push(todo);
       });
     };
   });
